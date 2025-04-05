f02d69a5cd86bbe9998aef507e1d73375fb7a9fa
Mohayminul Mukit <117625337+mukit-blaster@users.noreply.github.com>
2025-04-06 00:23:30 +0600

    update port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const cors = require('cors');
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Connect to MongoDB Atlas
 mongoose.connect(process.env.MONGODB_URI)
